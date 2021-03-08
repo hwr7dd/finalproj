f7f2ca63b72824cfa7ce50a7430f2a20b9c7e3bc
Harrison <55821406+hwr7dd@users.noreply.github.com>
2021-03-08 00:01:11 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
+import './App.css'
+import useDataApi from 'use-data-api';
 
 function App() {
+  //This part fetches the data from the API provided by coingecko
+  const [query, setQuery] = useState('bitcoin');
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
+    { hits: [] },
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h2>
+          Cryptocurrency Price Check
+          </h2> <div className></div><span role="img" className='emoji'aria-label="money">💵</span>
+
+          <h6>Enter your crypto below (ex: bitcoin, ethereum, cardano):</h6>     
+          </div>
+    <Fragment>
+      <div className='form'>
+        {/*This form changes the parameter of the api query dynamically*/}
+      <form
+        onSubmit={event => {
+      {/*When user submits, the portion ${query} is changed to their term*/}
+
+          doFetch(
+            `https://api.coingecko.com/api/v3/simple/price?ids=${query}&vs_currencies=usd`,
+          );
+
+          event.preventDefault();
+          console.log(data)
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button className= 'buttn'type="submit">Search</button>
+      </form>
+      </div>
+      {/*error handling*/}
+      {isError && <div>Something went wrong ...</div>}
+<div>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div className='response'> <h1>{query}</h1>
+{JSON.stringify(data)}
+        </div>
+      )}
+      </div>
+    </Fragment>
     </div>
   );
 }
